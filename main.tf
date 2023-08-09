533860a22dedd47380d54e1109b14a220177780a
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2023-08-09 08:08:07 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+variable "TFC_CONFIGURATION_VERSION_GIT_TAG" {
+  default = ""
+}
+
 resource "random_pet" "name" {
  length    = "7"
  separator = "-"
@@ -7,6 +11,15 @@ resource "random_pet" "name" {
 resource "null_resource" "env" {
   provisioner "local-exec" {
     command = "env"
+  }
+  triggers = {
+    run_every_time = uuid()
+  }
+}
+
+resource "null_resource" "env" {
+  provisioner "local-exec" {
+    command = "echo ${var.TFC_CONFIGURATION_VERSION_GIT_TAG}"
   }
   triggers = {
     run_every_time = uuid()
